fa7e55ae2e9ffd6ce6742dcc7e95b426f6424826
andrkh13 <Andreas@Andreass-MacBook-Pro.local>
2013-10-11 10:56:27 +0200

    LAB 2 Siste

---
+++ Lab2.js
@@ -0,0 +1,5 @@
+﻿function printToOutputBox(selectobj){
+var e_proglangs = document.getElementById("proglangs");
+var e_outputbox = document.getElementById("outputbox");
+e_outputbox.innerHTML = "Du har selektert programmeringsspr&aringket " + e_proglangs.options[selectobj.selectedIndex].text;
+}
